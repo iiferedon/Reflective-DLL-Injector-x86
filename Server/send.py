6b3821ee64d34e73c8daf9250a181d50b76ff151
iiferedon <97226864+iiferedon@users.noreply.github.com>
2023-07-03 01:44:54 +1000

    Update send.py

--- Server/send.py
+++ Server/send.py
@@ -19,7 +19,7 @@ def stream_encrypted_data():
     encrypted_data = encrypt_data(DATA, KEY)
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Set SO_REUSEADDR option
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
     server_socket.bind(('0.0.0.0', 1222))
     server_socket.listen(1)
     print("Server started. Listening for incoming connections...")
